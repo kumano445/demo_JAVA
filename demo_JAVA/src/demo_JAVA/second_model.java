0b9b69f0002f4cc9409f69ea8650254158d42f26
yoshiko <ttkmysk021@gmail.com>
2024-01-14 10:01:21 +0900

    first commit two

---
+++ demo_JAVA/src/demo_JAVA/second_model.java
@@ -0,0 +1,29 @@
+package demo_JAVA;
+
+public class second_model {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int mylnt;
+		mylnt = 20;
+		double myDouble = mylnt;
+		
+		System.out.println("整数の初期化" + myDouble);
+		
+
+		char myChar;
+		myChar = 'K';
+		System.out.println("文字初期化" + myChar);
+		
+		boolean myBoolean;
+		myBoolean = true;
+		myBoolean = false;
+		
+		if (myBoolean) {
+			System.out.println("正です");
+		}else{
+			System.out.println("負です");
+		}
+	}
+
+}
