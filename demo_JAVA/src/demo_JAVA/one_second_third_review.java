29fe5d5bc5f3a234ddbe50e29b92d3f90647ff79
yoshiko <ttkmysk021@gmail.com>
2024-01-15 21:09:22 +0900

    first commit

---
+++ demo_JAVA/src/demo_JAVA/one_second_third_review.java
@@ -0,0 +1,18 @@
+package demo_JAVA;
+
+public class one_second_third_review {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int b = 15;
+		int h = 30;
+		int triangleArea = b * h / 2;
+		System.out.println(triangleArea);
+		
+		int r =  4;
+		double circleArea = r * r * 3.14;
+		System.out.println(circleArea);
+		
+	}
+
+}
